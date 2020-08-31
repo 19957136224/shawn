c5a00f24498f00d38dbd7c9410e1c252655f5b2a
liqing <2540931391@qq.com>
2020-08-31 10:23:37 +0800

    feat: push build/

---
+++ build/precache-manifest.5d95e122430bf5862cea15dc9f04b717.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f33ff41925a84b2e682ca8ee3b0763b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "298804a0429c3e1de7dd",
+    "url": "/static/css/2.847e01e0.chunk.css"
+  },
+  {
+    "revision": "05c948cd821e293a0588",
+    "url": "/static/css/main.4d8b63f9.chunk.css"
+  },
+  {
+    "revision": "298804a0429c3e1de7dd",
+    "url": "/static/js/2.392375a1.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.392375a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05c948cd821e293a0588",
+    "url": "/static/js/main.0ead4cfb.chunk.js"
+  },
+  {
+    "revision": "d14e1d4e18ac483e1043",
+    "url": "/static/js/runtime-main.75d070f0.js"
+  }
+]);
